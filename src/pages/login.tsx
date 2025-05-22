d2124fef8a1713c45977def0113957bd5b212672
BasieDEV <bashartkamp@gmail.com>
2025-05-22 09:58:00 +0200

    Added a couple pages

---
+++ src/pages/login.tsx
@@ -0,0 +1,32 @@
+export default function Home() {
+  return (
+    <div>
+        <header className="relative w-full h-auto">
+            <img src="/svg/bannermedium.svg" alt="Header Image" className="w-full h-auto" />
+            <div className="absolute inset-0 flex items-center justify-center">
+                <img src="/svg/Middlelogo.png" alt="Centered Logo" className="h-20 mt-21 w-auto" />
+            </div>
+        </header>
+        <div className="flex flex-col items-center justify-center">
+            <form className="mt-22" action="/api/login">
+                <div className="w-72 flex flex-col">
+                    <label className="text-xl ml-4" style={{ fontFamily: 'Londrina, Arial, Helvetica, sans-serif' }}>Gebruikersnaam</label>
+                    <input type="text" className="bg-[#F1DECD] h-12 rounded-4xl"/>
+                </div>
+                <div className="w-72 flex flex-col mt-8">
+                    <label className="text-xl ml-4" style={{ fontFamily: 'Londrina, Arial, Helvetica, sans-serif' }}>Wachtwoord</label>
+                    <input type="text" className="bg-[#F1DECD] h-12 rounded-4xl"/>
+                </div>
+                <div style={{ fontFamily: 'Londrina, Arial, Helvetica, sans-serif' }} className="mx-auto ml-4">
+                    Nog geen account? <a href="/register" className="text-[#F1DECD]" >Klik hier.</a>
+                </div>
+                <div>
+                    <button type="submit" className="bg-[#FBD064] text-[#fffff] text-xl h-12 w-36 mt-8 rounded-4xl float-right" style={{ fontFamily: 'Londrina, Arial, Helvetica, sans-serif', width: '50%' }}>
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+  );
+}
