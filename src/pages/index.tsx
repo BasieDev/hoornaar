e42eddabd918b35a9ebb5187b24bb40fe4e67d3f
MirtheVossestein <163519773+MirtheVossestein@users.noreply.github.com>
2025-05-26 13:41:43 +0200

    navbar working, pages made.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "@/components/navbar";
+import LogoSvg from "@/components/logo";
 
 
 const geistSans = Geist({
@@ -18,12 +19,9 @@ export default function Home() {
 
     <div className="home pb-[80px]">
       <Navbar />
-      <header className="relative w-full h-auto">
-        <img src="/svg/bannermedium.svg" alt="Header Image" className="w-full h-auto" />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <img src="/svg/Middlelogo.png" alt="Centered Logo" className="h-22 w-auto" />
-        </div>
-      </header>
+      <header className="LogoSvg">
+                  <LogoSvg />
+              </header>
     </div>
   );
 }
