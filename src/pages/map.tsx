eeceeb7cdfd6d38e9783bac6ddcb30785a3298db
MirtheVossestein <163519773+MirtheVossestein@users.noreply.github.com>
2025-06-18 11:18:41 +0200

    mapfilter

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -19,12 +19,10 @@ export default function Map() {
         <div className="home min-h-screen h-screen flex flex-col">
             <Navbar />
             <div className="relative flex-1 flex flex-col">
-                {/* LogoSvg is the wavy top background, absolutely positioned */}
                 <div className="absolute top-0 left-0 w-full z-20 pointer-events-none">
                     <LogoSvg />
                 </div>
                 <div className="flex-1 flex flex-col z-0 pt-[55px] pb-0 h-full min-h-0">
-                    {/* Make map stretch to fill all available vertical space */}
                     <OpenStreetMap />
                 </div>
             </div>
